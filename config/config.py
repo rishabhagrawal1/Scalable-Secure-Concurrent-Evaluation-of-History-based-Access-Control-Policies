3629cb3fd511bb85ac86f1eff48144f29b32f416
rishabh <you@example.com>
2016-12-07 00:04:08 -0500

    nost of the changes

--- config/config.py
+++ config/config.py
@@ -1,12 +1,12 @@
 import sys
 
 class Config:
-    def __init__(self, conf_data, config_file, policy_file, data_file, attr_file):
+    def __init__(self, conf_data, conf_file, policy_file, data_file, attr_file):
         self.conf_data = conf_data
         self.num_client = 1
         self.num_coordinator = 1
         self.num_workers = 1
-        self.config_file = config_file
+        self.conf_file = conf_file
         self.policy_file = policy_file
         self.data_file = data_file
         self.attr_file = attr_file
